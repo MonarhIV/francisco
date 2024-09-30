6995b08db98aa854ff075f5d61238263d5922068
Igor Tupichinskiy <tis22f158@student.bmstu.ru>
2024-09-30 23:10:43 +0300

    Test version of API

--- webapp/main.py
+++ webapp/main.py
@@ -1,36 +1,17 @@
+import asyncio
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 from fastapi import FastAPI
-from db import select_products_with_similar_name, Select_all_device
-import json
-import os
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-# async def convert_to_json(columns, data):
-#     for i in range(len(data)):
-#         data_dict = {}
-#         for j in range(len(columns)):
-#             data_dict[columns[j]] = data[i][j]
-#         data[i] = data_dict
-#     data = list(data)
-#     return json.dumps(data)
+from request import PostRequest
+from db import Select
 
 app = FastAPI(
     title="tt"
 )
 
 
-@app.get('/search')
-async def f(name: str):
-    data = await select_products_with_similar_name(name)
-    return json.dumps(data)
-
-
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get('/get_all')
-async def get_all():
-    data = await Select_all_device()
-    return json.dumps(data)
+@app.post('/api/search')
+async def f(request: PostRequest):
+    data = await Select(request.search)
+    print(data)
+    return {data}
