be677b85097ee8114ab75e0af4f6a4661fe3f9d3
Monarh <kostyukov311@gmail.com>
2024-10-01 17:03:55 +0300

    NORMAL MAIN

---
+++ client/francisco/src/pages/search/search.tsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+
+import Menu from '../../components/menu-nav/menu-nav';
+
+function Search(): JSX.Element {
+    return(
+        (
+            <section className="serch-page">
+                <div className="App">
+                    <Search />
+                </div>
+            </section>
+        )
+    );
+
+}
