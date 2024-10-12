f9ddb385a7510ce2c0532827e2fdf1b4e2d4c4d4
Monarh <kostyukov311@gmail.com>
2024-10-12 22:11:11 +0300

    fix

--- webapp/main.py
+++ webapp/main.py
@@ -25,7 +25,7 @@ DB_INFO = "dbname=Francisco_test user=postgres password=1234"
 
 @app.get('/search')
 async def f(name: str):
-    data = await db.select_products_with_similar_name(name)
+    data = await select_products_with_similar_name(name)
     return data
 
 
@@ -36,23 +36,23 @@ async def root():
 
 @app.get('/get_all')
 async def get_all():
-    data = await db.Select_all_device()
+    data = await Select_all_device()
     return data
 
 
 @app.get('/color_translator')
 async def get_col_name(hex_code):
-    name = await db.get_color_name(hex_code)
+    name = await get_color_name(hex_code)
     return name
 
 
 @app.get('/categories')
 async def get_kind(kind: str):
-    data = await db.select_category(kind)
+    data = await select_category(kind)
     return data
 
 
 @app.post('/update_price')
 async def update_pr(data=Body()):
-    res = await db.update_price(data["product_id"], data["new_price"])
+    res = await update_price(data["product_id"], data["new_price"])
     return json.dumps(res)
